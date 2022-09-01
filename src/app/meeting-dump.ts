306f05e256ef11e396cea5e0210c942d93f44b66
kevinnovianp <kevinnovianprafajar@gmail.com>
2022-09-02 02:10:43 +0700

    list w/ dump

--- src/app/meeting-dump.ts
+++ src/app/meeting-dump.ts
@@ -1,10 +1,10 @@
 import { Meeting } from "./meeting";
 
 export const MEETS:Meeting[] = [
-  // {id:1, startTime:'10:00', endTime:'11:30', date:'11-12-2022', title: 'Meet 1', desc: 'this is first meet'},
-  // {id:2, startTime:'10:00', endTime:'11:30', date:'12-12-2022', title: 'Meet 2', desc: 'this is second meet'},
-  // {id:3, startTime:'10:00', endTime:'11:30', date:'13-12-2022', title: 'Meet 3', desc: 'this is third meet'},
-  // {id:1, startTime:'10:00', endTime:'11:30', date:'11-12-2022', title: 'Meet 1', desc: 'this is first meet'},
-  // {id:2, startTime:'10:00', endTime:'11:30', date:'12-12-2022', title: 'Meet 2', desc: 'this is second meet'},
-  // {id:3, startTime:'10:00', endTime:'11:30', date:'13-12-2022', title: 'Meet 3', desc: 'this is third meet'},
+  {id:1, startTime:'10:00', endTime:'11:30', date:'11-12-2022', title: 'Meet 1', desc: 'This is first meet'},
+  {id:2, startTime:'10:00', endTime:'11:30', date:'12-12-2022', title: 'Meet 2', desc: 'This is second meet'},
+  {id:3, startTime:'10:00', endTime:'11:30', date:'13-12-2022', title: 'Meet 3', desc: 'This is third meet'},
+  {id:1, startTime:'10:00', endTime:'11:30', date:'11-12-2022', title: 'Meet 1', desc: 'This is first meet'},
+  {id:2, startTime:'10:00', endTime:'11:30', date:'12-12-2022', title: 'Meet 2', desc: 'This is second meet'},
+  {id:3, startTime:'10:00', endTime:'11:30', date:'13-12-2022', title: 'Meet 3', desc: 'This is third meet'},
 ]
