202c63a678c24bf7547a9c984b741ea1b9881ef0
kevinnovianp <kevinnovianprafajar@gmail.com>
2022-09-01 12:09:21 +0700

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FormComponent } from './form/form.component';
+import { ListComponent } from './list/list.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'new_meeting', pathMatch: 'full'},
+  {path: 'new_meeting', component: FormComponent},
+  {path: 'view_meetings', component: ListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [FormComponent, ListComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FormComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
