e7fe5ef091fec5d0f927e8018b1345a258076c14
kevinnovianp <kevinnovianprafajar@gmail.com>
2022-09-02 16:46:31 +0700

    date + time format

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -14,21 +14,38 @@ import { Router } from '@angular/router';
 
 export class FormComponent implements OnInit {
 
-  public id!: number
-  public startTime!: string
-  public endTime!: string
-  public date!: string
-  public title!: string
-  public desc!: string
+  // public id!: number
+  // public startTime!: string
+  // public endTime!: string
+  // public date!: string
+  // public title!: string
+  // public desc!: string
 
   constructor() { }
 
   currId=1
-  // newMeet=new Meeting()
+  newMeet=new Meeting()
+  month=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+
+  public changeDateFormat(date:any){
+    var od = new Date(date)
+    var nd = od.getDate() + " " + this.month[od.getMonth()] + " " + od.getFullYear()
+    return nd
+  }
+
+  public changeTimeFormat(time:any){
+    var hour = Number(time.substring(0,2))
+    var local = hour<12 ? "AM" : "PM"
+    hour = hour>12 ? hour-12 : hour
+    var minute = Number(time.substring(3,5))
+    var nt = String(hour).padStart(2, '0') + ":" + String(minute).padStart(2, '0') + " " + local
+    return nt
+  }
 
   submitForm(){
-    this.id = this.currId++
-    alert(this.id + "; " + this.title + "; " + this.date + "; "+ this.startTime + " s/d " + this.endTime + "; " + this.desc)
+    this.newMeet.id = this.currId++
+
+    alert(this.newMeet.id + "; " + this.newMeet.title + "; " + this.changeDateFormat(this.newMeet.date) + "; "+ this.changeTimeFormat(this.newMeet.startTime) + " s/d " + this.changeTimeFormat(this.newMeet.endTime) + "; " + this.newMeet.desc)
     location.pathname = ('/view_meetings');
   }
 
--- src/app/meeting-dump.ts
+++ src/app/meeting-dump.ts
@@ -1,10 +1,10 @@
 import { Meeting } from "./meeting";
 
-export const MEETS:Meeting[] = [
-  // {id:1, startTime:'10:00', endTime:'11:30', date:'11-12-2022', title: 'Meet 1', desc: 'This is first meet'},
-  // {id:2, startTime:'10:00', endTime:'11:30', date:'12-12-2022', title: 'Meet 2', desc: 'This is second meet'},
-  // {id:3, startTime:'10:00', endTime:'11:30', date:'13-12-2022', title: 'Meet 3', desc: 'This is third meet'},
-  // {id:4, startTime:'10:00', endTime:'11:30', date:'11-12-2022', title: 'Meet 4', desc: 'This is fourth meet'},
-  // {id:5, startTime:'10:00', endTime:'11:30', date:'12-12-2022', title: 'Meet 5', desc: 'This is fifth meet'},
-  // {id:6, startTime:'10:00', endTime:'11:30', date:'13-12-2022', title: 'Meet 6', desc: 'This is sixth meet'},
+export var MEETS:Meeting[] = [
+  {id:1, startTime:'10:00 AM', endTime:'11:30 AM', date:'11 Jan 2022', title: 'Meet 1', desc: 'This is first meet'},
+  {id:2, startTime:'11:00 AM', endTime:'11:30 AM', date:'12 Aug 2022', title: 'Meet 2', desc: 'This is second meet'},
+  {id:3, startTime:'12:00 AM', endTime:'01:30 PM', date:'13 May 2022', title: 'Meet 3', desc: 'This is third meet'},
+  {id:4, startTime:'03:00 PM', endTime:'04:30 PM', date:'11 Dec 2022', title: 'Meet 4', desc: 'This is fourth meet'},
+  {id:5, startTime:'10:00 AM', endTime:'11:30 AM', date:'14 Jul 2022', title: 'Meet 5', desc: 'This is fifth meet'},
+  {id:6, startTime:'11:30 AM', endTime:'01:30 PM', date:'13 May 2022', title: 'Meet 6', desc: 'This is sixth meet'},
 ]
