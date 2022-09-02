8797c1788efbfd8297092ad9edaaec612fb04319
kevinnovianp <kevinnovianprafajar@gmail.com>
2022-09-02 10:17:46 +0700

    collapse btn

--- src/app/meeting-dump.ts
+++ src/app/meeting-dump.ts
@@ -4,7 +4,7 @@ export const MEETS:Meeting[] = [
   {id:1, startTime:'10:00', endTime:'11:30', date:'11-12-2022', title: 'Meet 1', desc: 'This is first meet'},
   {id:2, startTime:'10:00', endTime:'11:30', date:'12-12-2022', title: 'Meet 2', desc: 'This is second meet'},
   {id:3, startTime:'10:00', endTime:'11:30', date:'13-12-2022', title: 'Meet 3', desc: 'This is third meet'},
-  {id:1, startTime:'10:00', endTime:'11:30', date:'11-12-2022', title: 'Meet 1', desc: 'This is first meet'},
-  {id:2, startTime:'10:00', endTime:'11:30', date:'12-12-2022', title: 'Meet 2', desc: 'This is second meet'},
-  {id:3, startTime:'10:00', endTime:'11:30', date:'13-12-2022', title: 'Meet 3', desc: 'This is third meet'},
+  {id:4, startTime:'10:00', endTime:'11:30', date:'11-12-2022', title: 'Meet 4', desc: 'This is fourth meet'},
+  {id:5, startTime:'10:00', endTime:'11:30', date:'12-12-2022', title: 'Meet 5', desc: 'This is fifth meet'},
+  {id:6, startTime:'10:00', endTime:'11:30', date:'13-12-2022', title: 'Meet 6', desc: 'This is sixth meet'},
 ]
