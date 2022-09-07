32f23286e7fca6ef7be29edaa709f9a41465ed0d
kevinnovianp <kevinnovianprafajar@gmail.com>
2022-09-07 16:00:35 +0700

    ..

--- src/app/meeting.service.ts
+++ src/app/meeting.service.ts
@@ -10,17 +10,21 @@ export class MeetingService {
   private meetings: BehaviorSubject<Meeting[]> = new BehaviorSubject<Meeting[]>([
     {id:1, startTime:'10:00', endTime:'11:30', date:'2022-01-11', title: 'Meet 1', desc: 'This is first meet'},
     {id:2, startTime:'11:00', endTime:'11:30', date:'2022-08-12', title: 'Meet 2', desc: 'This is second meet'},
-    {id:3, startTime:'12:00', endTime:'13:30', date:'2022-05-13', title: 'Meet 3', desc: 'This is third meet'},
+    // {id:3, startTime:'12:00', endTime:'13:30', date:'2022-05-13', title: 'Meet 3', desc: 'This is third meet'},
     // {id:4, startTime:'15:00', endTime:'16:30', date:'2022-12-11', title: 'Meet 4', desc: 'This is fourth meet'},
     // {id:5, startTime:'10:00', endTime:'11:30', date:'2022-07-14', title: 'Meet 5', desc: 'This is fifth meet'},
     // {id:6, startTime:'11:30', endTime:'16:30', date:'2022-05-13', title: 'Meet 6', desc: 'This is sixth meet'}
   ])
-  private currId = 3
+  private currId = this.getMeetingsLength()
 
   constructor() { }
 
   getAllMeetings(): Observable<Meeting[]> {
     return this.meetings;
+  }
+
+  getMeetingsLength(){
+    return this.meetings.value.length
   }
 
   pushMeeting(newMeeting: any) {
